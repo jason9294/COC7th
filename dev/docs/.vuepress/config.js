30498ec8e634e7de5315e61fd12aea221e6931c8
jason9294 <63494593+jason9294@users.noreply.github.com>
2022-04-24 21:44:04 +0800

    .

---
+++ dev/docs/.vuepress/config.js
@@ -0,0 +1,58 @@
+module.exports = {
+    title: 'COC7版規則 廚房筆記',
+    description: 'COC7th 規則筆記',
+    base: '/COC7th/',
+
+    plugins: ['@vuepress/back-to-top'],
+
+    markdown: {
+        lineNumbers: true
+    },
+    
+    themeConfig: {
+        smoothScroll: true,
+        nextLinks: false,
+        prevLinks: false,
+        sidebar: {
+            '/guide/': [
+                '',
+              {
+                title: '每日功能',
+                //path: '/guide/daily/',      // 可选的
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    '/guide/daily/1A2B',
+                    '/guide/daily/covid-19',
+                ]
+              },
+              {
+                title: '自訂義回覆',
+                collapsable: false,
+                sidebarDepth: 0,
+                children: [
+                    '/guide/custom_reply/dictionary',
+                    '/guide/custom_reply/keyword'
+                ]
+              },
+              {
+                title: 'TRPG',
+                collapsable: false,
+                sidebarDepth: 0,
+                children: [
+                    '/guide/trpg/dice',
+                ]
+              }
+            ],
+
+            // fallback
+            '/': [
+              '/intro',        /* / */
+            ]
+          },
+
+        nav: [
+            { text: 'Home', link: '/' }
+        ]
+    }
+}
---
+++ dev/docs/readme.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: logo.png
+heroText: COC7版規則整理
+tagline: 心結廚房製作
+actionText: 開始 →
+actionLink: /intro
+---
